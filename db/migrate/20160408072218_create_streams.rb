0b6009ada24def7cb21d54a8f5b4731f26ff1b3d
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-08 19:41:25 +0300

    Add Stream#user_id

--- db/migrate/20160408072218_create_streams.rb
+++ db/migrate/20160408072218_create_streams.rb
@@ -1,6 +1,7 @@
 class CreateStreams < ActiveRecord::Migration
   def change
     create_table :streams do |t|
+      t.integer :user_id, index: true
       t.string :title
       t.text :desc
       t.string :status
