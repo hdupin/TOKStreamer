47c7e284372996b0892fc60cb24c9c5bce96b3fd
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-08 19:43:04 +0300

    Add views and JS code to allow users start broadcasting!

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,21 +1,64 @@
 class StreamsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
 
   end
 
+  def new
+
+  end
+
+  def show
+    populate_gon_vars
+  end
+
+  def create
+    if create_stream_with_tok_session_service
+      redirect_to stream, notice: 'Nice! You are ready to start a stream!'
+    else
+      flash.now[:alert] = 'Please fix these errors first: %s' % stream.errors.full_messages.first
+      render :new
+    end
+  end
+
   private
 
-  def streams
-    stream = Struct.new(:title, :desc) do
-      def to_partial_path
-        'stream'
-      end
+  def create_stream_with_tok_session_service
+    session = OpenTokClient.create_session
+    stream.session_id = session.session_id
+    stream.save
+  end
 
-      def to_model
-        Stream.new
-      end
-    end
-    10.times.map {|i| stream.new("Stream #{i}", "Hey! Check out this steam!")}
+  def stream
+    @stream ||= if params[:id]
+                  (current_user ? current_user.streams : Stream).find(params[:id])
+                else
+                  current_user.streams.new(stream_params)
+                end
+  end
+  helper_method :stream
+
+  def streams
+    @streams ||= Stream.all
   end
   helper_method :streams
+
+  def stream_params
+    params.fetch(:stream, {}).permit(:title, :desc)
+  end
+
+  def populate_gon_vars
+    role = stream_role_for_user(user: current_user, stream: stream)
+    gon.opentok = {
+        sessionId: stream.session_id,
+        apiKey: Figaro.env.opentok_api_key,
+        token: OpenTokClient.generate_token(stream.session_id, {role: role}),
+        role: role
+    }
+  end
+
+  def stream_role_for_user(stream:, user:)
+    user&.id == stream.user_id ? :publisher : :subscriber
+  end
+
 end
