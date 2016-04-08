cc54c7ce66384c7181d16babf787d8c83902cc52
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-08 13:04:12 +0300

    Added basic mark-up and mocks for future streams

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,2 +1,21 @@
 class StreamsController < ApplicationController
+  def index
+
+  end
+
+  private
+
+  def streams
+    stream = Struct.new(:title, :desc) do
+      def to_partial_path
+        'stream'
+      end
+
+      def to_model
+        Stream.new
+      end
+    end
+    10.times.map {|i| stream.new("Stream #{i}", "Hey! Check out this steam!")}
+  end
+  helper_method :streams
 end
