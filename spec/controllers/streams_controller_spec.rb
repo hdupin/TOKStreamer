d038104463cc2e0ee15f3dd70b0c09188749a539
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-10 21:34:35 +0300

    Add specs for streams controller

--- spec/controllers/streams_controller_spec.rb
+++ spec/controllers/streams_controller_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe StreamsController, type: :controller do
       expect(response).to be_ok
     end
   end
+
+  describe "GET show" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:stream) { FactoryGirl.create(:stream, user: user, session_id: 1, title: 'Stream') }
+    let(:gon) { RequestStore.store[:gon].gon }
+
+    before do
+      sign_in user
+      allow(OpenTokClient).to receive(:generate_token){ 'token' }
+    end
+
+    it "populates gon vars" do
+      get :show, {id: stream.id}
+      expect(gon['opentok']).to be_present
+    end
+  end
 end
