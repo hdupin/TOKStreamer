19f731df468ee66c785a33e5c324ad40d2014498
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-10 13:42:32 +0300

    Add ENV flag to run coverage locally

--- spec/coverage_reporters.rb
+++ spec/coverage_reporters.rb
@@ -4,7 +4,9 @@ require "codeclimate-test-reporter"
 
 reporters = [
     (Coveralls::SimpleCov::Formatter if Coveralls.will_run?),
-    (CodeClimate::TestReporter::Formatter if CodeClimate::TestReporter.run?)
+    (CodeClimate::TestReporter::Formatter if CodeClimate::TestReporter.run?),
+    (SimpleCov::Formatter::HTMLFormatter if ENV['COV'])
 ].compact
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(reporters)
 SimpleCov.start 'rails' unless reporters.empty?
