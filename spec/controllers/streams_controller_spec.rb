1f07a5e55f37a86dd5c9c4607a7e4b243127b26d
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-04-10 12:39:34 +0300

    Add specs for Stream

--- spec/controllers/streams_controller_spec.rb
+++ spec/controllers/streams_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StreamsController, type: :controller do
-
+  describe "GET index" do
+    it "returns OK status" do
+      get :index
+      expect(response).to be_ok
+    end
+  end
 end
--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Stream, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "requires user to be set" do
+      stream = FactoryGirl.build(:stream, user: nil)
+      expect(stream.tap(&:valid?).errors[:user].size).to eql(1)
+    end
+
+    it "requires session_id to be set" do
+      stream = FactoryGirl.build(:stream, session_id: nil)
+      expect(stream.tap(&:valid?).errors[:session_id].size).to eql(1)
+    end
+
+    it "requires title to be set" do
+      stream = FactoryGirl.build(:stream, title: nil)
+      expect(stream.tap(&:valid?).errors[:title].size).to eql(1)
+    end
+  end
+
 end
