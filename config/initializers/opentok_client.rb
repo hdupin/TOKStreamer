8a8eae7cdcf5d644c173183891a2a44a0de8d9ac
hygordupin <hygorburza@gmail.com>
2017-07-31 21:29:03 -0300

    Prepare to Heroku

--- config/initializers/opentok_client.rb
+++ config/initializers/opentok_client.rb
@@ -1,1 +1,4 @@
-OpenTokClient = OpenTok::OpenTok.new Figaro.env.opentok_api_key, Figaro.env.opentok_api_secret
+api_key = "45922822"        # Replace with your OpenTok API key.
+api_secret = "8f6fb4e01aff3c833d89f10cdb1cf62e6332e029"  # Replace with your OpenTok API secret.
+
+OpenTokClient = OpenTok::OpenTok.new api_key, api_secret
